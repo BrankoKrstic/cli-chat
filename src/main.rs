2524f3c5240919ebc41e5d1c49f3067ac6c3e816
brankokrstic94 <brankokrstic94@gmail.com>
2023-12-27 17:51:22 +0100

    basic binary setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::{io::AsyncWriteExt, net::TcpListener};
+
+type ChatResult<T> = Result<T, Box<dyn std::error::Error>>;
+
+#[tokio::main]
+async fn main() -> ChatResult<()> {
+    let addr = "127.0.0.1";
+    let port = 8080;
+    let listener = TcpListener::bind((addr, port))
+        .await
+        .map_err(|e| format!("Failed to bind to socket {addr} {}", e))?;
+
+    loop {
+        match listener.accept().await {
+            Ok((mut socket, _)) => {
+                socket.write(b"Hello, brother").await;
+            }
+            Err(e) => eprintln!("failed connecting to client: {}", e),
+        }
+    }
 }
